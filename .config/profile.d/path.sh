486d2879bf6ba8a9f5b6e209ef6c5d58c677a817
John Sullivan <jsullivan@csumb.edu>
2020-01-19 03:24:35 -0800

    Trimming non-determinism

--- .config/profile.d/path.sh
+++ .config/profile.d/path.sh
@@ -1,21 +1,4 @@
 LOCAL=$HOME/.local
 SHARE=$LOCAL/share
 
-# JAVA
-export JAVA_HOME=$SHARE/jdk/jdk-11.0.4+11
-
-# Droid
-export ANDROID_SDK_ROOT=$HOME/Android/Sdk
-
-# Path finagling
-EXTPATH=($JAVA_HOME/bin \
-         $ANDROID_SDK_ROOT/tools \
-         $ANDROID_SDK_ROOT/platform-tools \
-         $SHARE/android-studio/bin \
-         $SHARE/scala/dotty/bin \
-         $LOCAL/bin \
-         $HOME/.npm-global/bin \
-         $HOME/.gem/ruby/2.6.0/bin \
-         $HOME/.config/composer/vendor/bin)
-
-export PATH=$(printf "%s:" ${EXTPATH[@]} $PATH)
+export PATH="$PATH:$LOCAL/bin"
--- .config/profile.d/xorg.sh
+++ .config/profile.d/xorg.sh
@@ -1,5 +1,4 @@
 export XORG_PROFILE="$HOME/.config/xprofile.d/bspwm.sh"
-export QT_STYLE_OVERRIDE=adwaita
 
 if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
   exec startx
