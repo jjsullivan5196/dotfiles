8961494676d79cc0bfa5e2852e81599feaae87de
John Sullivan <jsullivan@csumb.edu>
2020-08-20 23:22:46 -0700

    dock script

---
+++ .local/bin/dock.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export SWAYSOCK=${XDG_RUNTIME_DIR}/sway-*.sock
+HDMI_CONNECTED=`cat /sys/class/drm/card0-HDMI-A-1/status`
+
+# Check if HDMI is connected
+if [ "${HDMI_CONNECTED}" = "connected" ]; then
+  # Change display
+  swaymsg output DSI-1 disable
+  swaymsg output HDMI-A-1 enable
+
+  # Change default audio sink
+  pacmd 'set-default-sink 1'
+else
+  echo
+  # Change display
+  swaymsg output HDMI-A-1 disable
+  swaymsg output DSI-1 enable
+
+  # Change default audio sink
+  pacmd 'set-default-sink 0'
+fi
