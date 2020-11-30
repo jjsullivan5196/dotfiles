9ca63983e82ec5b92132989c93f715176ba08842
John Sullivan <jsullivan@csumb.edu>
2020-11-30 15:30:15 -0800

    php

--- .config/profile.d/path.sh
+++ .config/profile.d/path.sh
@@ -4,4 +4,4 @@ SHARE=$LOCAL/share
 export GRAALVM_HOME="$SHARE/graalvm-ce-java11-20.2.0"
 export JAVA_HOME=${GRAALVM_HOME}
 
-export PATH="$PATH:$LOCAL/bin:$JAVA_HOME/bin:$SHARE/maven/bin:$SHARE/clojure/bin:$HOME/.yarn/bin"
+export PATH="$PATH:$LOCAL/bin:$JAVA_HOME/bin:$SHARE/maven/bin:$SHARE/clojure/bin:$HOME/.yarn/bin:$HOME/.config/composer/vendor/bin"
