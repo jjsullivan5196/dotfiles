81c86385496b2a12aade7f05a9e13893c351cf6f
John Sullivan <jsullivan@csumb.edu>
2021-12-24 03:07:04 -0800

    improve dotfiles init script

--- .local/lib/scripts/init-dotfiles.sh
+++ .local/lib/scripts/init-dotfiles.sh
@@ -1,8 +1,8 @@
 ORIGIN_URL="git@github.com:jjsullivan5196/dotfiles.git"
 
 cd ${HOME}
-
-git init
-git remote add origin "${ORIGIN_URL}"
-git fetch
-git checkout -f master
+git init && \
+git remote add origin "${ORIGIN_URL}" && \
+git fetch && \
+git checkout -f master && \
+git submodule update --init --recursive
