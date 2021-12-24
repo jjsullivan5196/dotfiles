c968988da5f59a50602a6f52ffbe95dd84a6c105
John Sullivan <jsullivan@csumb.edu>
2021-12-23 23:42:15 -0800

    goodbye x

--- .config/profile.d/99-xorg.sh
+++
@@ -1,6 +0,0 @@
-export XINITRC=${XDG_CONFIG_HOME}/X11/xstart
-export XSERVERRC=${XDG_CONFIG_HOME}/X11/xserverrc
-export USERXSESSION=${XDG_CONFIG_HOME}/X11/xsession
-export XAUTHORITY=${XDG_RUNTIME_DIR}/X11/Xauthority
-
-[[ ! $DISPLAY && $XDG_VTNR -eq 1 ]] && exec xinit
