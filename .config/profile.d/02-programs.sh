8fda7788744028c1cf66d9932b8fb66f0722e692
John Sullivan <jsullivan@csumb.edu>
2021-12-23 23:38:17 -0800

    browser and dmenu options

--- .config/profile.d/02-programs.sh
+++ .config/profile.d/02-programs.sh
@@ -2,5 +2,6 @@ export ALTERNATE_EDITOR=""
 export EDITOR='emacsclient -c'
 export VISUAL="$EDITOR -n"
 export TERMINAL="foot"
-export BROWSER="chromium"
+export BROWSER="firefox"
 export GRIM_DEFAULT_DIR="$HOME/pics/screen/"
+export BEMENU_OPTS="-i -l 10"
