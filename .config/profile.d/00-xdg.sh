5848a5de43681bf5cd36a34ab2069a379ca2e9ad
John Sullivan <jsullivan@csumb.edu>
2022-01-11 11:04:52 -0800

    prob not the right way to do this

--- .config/profile.d/00-xdg.sh
+++ .config/profile.d/00-xdg.sh
@@ -1,4 +1,3 @@
-export XDG_RUNTIME_DIR=$(${HOME}/.local/lib/scripts/xdg-runtime-dir)
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
@@ -9,7 +8,6 @@ export PASSWORD_STORE_DIR="$XDG_DATA_HOME"/password-store
 export LESSHISTFILE=-
 #export XINITRC="$XDG_CONFIG_HOME"/X11/xinitrc
 #export XSERVERRC="$XDG_CONFIG_HOME"/X11/xserverrc
-
 
 export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
 
